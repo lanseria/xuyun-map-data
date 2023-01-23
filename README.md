b2eb3fe2ddf69e7a7b881ee06e1ceb32696ae950
lanseria <zhangchao564265135@hotmail.com>
2023-01-23 21:21:34 +0800

    add desc

---
+++ README.md
@@ -0,0 +1,5 @@
+# 徐云流浪中国地图开源数据
+
+[相关网站 lanseria/xuyun-travel-map](https://xuyun-map.netlify.app/)
+
+[数据格式解释](./src/types.ts)
--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,44 @@
 export interface PointFeatureProp {
+  // 自动生成
   id: string
+  // 切片点描述
   name: string
+  // 切片点日期
   date: string
+  // 切片点时间（大致）
   time: string
+  // 自动生成
   icon: string
+  // 视频ID
   vid: string
+  // 视频名称
   vName: string
+  // 视频切片秒数
   vt: number
+  // 视频发布时间
   vDate: string
 }
 
 export interface RawItemData {
+  // 切片点描述
   name: string
+  // 切片点日期
   date: string
+  // 切片点时间（大致）
   time: string
   type: 'bicycle' | 'campsite'
+  // 切片点坐标（大致）
   coordinates: [number, number]
+  // 切片点时间（大致）
   vTime: number
 }
 
 export interface RawData {
+  // 视频ID
   vid: string
+  // 视频发布时间
   vDate: string
+  // 视频名称
   vName: string
   vClips: RawItemData[]
 }
